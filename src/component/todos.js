db9514c085b264a3603f958bb8c6050683f33603
Phillip Dinh <phillipdinh04@gmail.com>
2024-07-22 21:33:03 -0400

    finished lab

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material";
+import { Card, Grid, ListItemButton, ListItemText, Checkbox } from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -13,19 +13,19 @@ const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       let color = "#ffffffff"
-      if (new Date(todo.duedate) < new Date()){
+      if (new Date(todo.duedate) < new Date()) {
         color = 'red'
       }
       console.log(color)
       return (
         <Grid key={todo.id}>
-          <Card data-testid={todo.content} style={{marginTop:10, background: color}}>
+          <Card data-testid={todo.content} style={{ marginTop: 10, background: color }}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}
             <ListItemButton component="a" href="#simple-list">
-              <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
-              <ListItemText primary={todo.content} secondary={todo.duedate.toLocaleString("en-US")}/>
+              <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={() => deleteTodo(todo.id)} />
+              <ListItemText primary={todo.content} secondary={todo.duedate.toLocaleString("en-US")} />
             </ListItemButton>
           </Card>
         </Grid>
